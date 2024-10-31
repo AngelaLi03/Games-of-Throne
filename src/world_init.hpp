21a8bed977649f67dfa7d1cd5a48188a46ee3445
ljyt03 <litoby1203@gmail.com>
2024-10-30 21:37:16 -0700

    larger map + new rooms

--- src/world_init.hpp
+++ src/world_init.hpp
@@ -38,3 +38,5 @@ Entity createWeapon(RenderSystem* renderer, vec2 pos);
 Entity createHealthBar(RenderSystem* renderer, vec2 pos, Entity owner_entity);
 
 Entity createFlowMeter(RenderSystem *renderer, vec2 pos, float scale);
+
+//void createRoom(RenderSystem* renderer, WallMap& wallMap, int x_start, int y_start, int width, int height, float tile_scale);
